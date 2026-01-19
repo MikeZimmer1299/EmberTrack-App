9c58407f9d39936f4d3b0b364fe9ff2eaac073de
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-01-19 12:48:08 -0500

    Changed the MyCigars from file to class

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/MyCigars.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/MyCigars.kt
@@ -4,28 +4,31 @@ import androidx.compose.runtime.Composable
 import com.example.cigarsandwhiskey.MainActivity
 
 data class MyCigars(
-    val cigarBrand: String,
-    val cigarName: String,
-    val sizeLength: Int,
-    val ringGauge: Int,
-    val humidor: String
+    var cigarBrand: String,
+    var cigarName: String,
+    var sizeLength: Float,
+    var ringGauge: Int,
+    var humidor: String
+    // TODO: At later time, may decide to return all data classes to be their own classes
+    //  Unknown, as of right now, if MyCigars best left as data class or an entire class with
+    //  included functions
+    // The main issue is simply because if I need to call an object inside the screen, I will
+    //  to import the package into the screens
 )
 
 class CigarMainActivity(){
 
+    // I think I can technically remove this function, since now all screens
+    //  have their own files to handle the display (and eventually handle all accessed data)
 
-
-    fun MainView(){
-
-    }
 }
 
 @Composable
-fun filterList(){
+fun filterList(list: List<MyCigars>){
 
 }
 
 @Composable
 fun MyCigarsScreen(){
-    filterList()
+//    filterList()
 }
