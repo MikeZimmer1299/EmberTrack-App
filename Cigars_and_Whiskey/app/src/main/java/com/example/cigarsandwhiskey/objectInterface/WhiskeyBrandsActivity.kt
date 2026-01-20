70881ff718199b2bda03e7788fcff2d02f4abe2d
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-01-20 10:21:07 -0500

    Added the basic background color and ability to scroll on screens to align with HomeScreen

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/WhiskeyBrandsActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/WhiskeyBrandsActivity.kt
@@ -2,10 +2,14 @@ package com.example.cigarsandwhiskey.objectInterface
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -17,6 +21,10 @@ fun WhiskeyBrandsScreen(){
         modifier = Modifier
             .fillMaxSize()
             .padding(0.dp, 0.dp)
+            .verticalScroll(rememberScrollState()),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFF37534E)
+        )
     ){
         Text(text = "Welcome to the Whiskey Brands Page")
     }
