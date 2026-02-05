fe19ddb214c6c4ab3e28afc9524d50e384db5613
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-05 10:40:25 -0500

    Changed var types, added comment

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/MyWhiskey.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/MyWhiskey.kt
@@ -3,13 +3,14 @@ package com.example.cigarsandwhiskey.objects
 import androidx.compose.runtime.Composable
 
 data class MyWhiskey(
-    val brand: String,
-    val name: String,
-    val proof: Float
+    var brand: String,
+    var name: String,
+    var proof: Float
 )
 
 @Composable
 // param proof = the proof of the chosen whiskey to be converted
+// When entering a float, remember to add `f` at the end if a hard int or `.toFloat()` if val/var
 fun proofToPercent(proof: Float): Float {
     return (proof / 2);
 }
