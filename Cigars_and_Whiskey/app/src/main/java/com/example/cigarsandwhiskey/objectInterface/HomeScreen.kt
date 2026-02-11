620a3469a6447963b7433db7880fa77e47f65c42
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-10 20:40:54 -0500

    A lot of additions to the Home Screen. Also a few different commented out sections of code where I was testing my object creation and accessing them (probably should move this into its own separate test file). Added different cards with their narrowed down purposes, along with the general formatting of the cards (will change as the specifics are narrowed down, may become slightly bigger)

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/HomeScreen.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/HomeScreen.kt
@@ -1,26 +1,26 @@
 package com.example.cigarsandwhiskey.objectInterface
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cigarsandwhiskey.objects.MyCigars
+import com.example.cigarsandwhiskey.objects.MyWhiskey
+import com.example.cigarsandwhiskey.objects.filterList
+import com.example.cigarsandwhiskey.objects.proofToPercent
 import com.example.cigarsandwhiskey.ui.theme.*
 
 
@@ -34,8 +34,6 @@ import com.example.cigarsandwhiskey.ui.theme.*
 @Preview
 fun HomeScreen(){
 
-//    Text(text= "Welcome to the Home Page")
-
     // TODO: Need to think about what type of information should be on the home screen
 
     // TODO: Within Card, since I plan to be able to scroll, I will need
@@ -47,12 +45,7 @@ fun HomeScreen(){
             .padding(0.dp, 0.dp)
             .verticalScroll(rememberScrollState()),
         colors = CardDefaults.cardColors(
-//            containerColor = lushForestGreenDark
-//            containerColor = Color(0xFF2b3a3f)
-//            containerColor = Color(0xFF554e4a)
-//            containerColor = Color(0xFF927E83)
-            containerColor = Color(0xFF37534E)
-//            containerColor = Color(0xFFC8BCAB)
+            containerColor = lushForestGreenDark
         )
         // TODO: look at Theme.kt to figure out what each color is getting its params from
     ){
@@ -61,8 +54,9 @@ fun HomeScreen(){
 
         // Row for each object desired on the home screen
         // TODO: May make these, instead of rows, as other cards? Example below of elevated card
+        //  This is the correct move
         ElevatedCard(
-            onClick = {},
+//            onClick = {},
             modifier = Modifier
                 .padding(
                     10.dp, // left
@@ -73,13 +67,94 @@ fun HomeScreen(){
 //                .background(color = Color.Green)
                 .size(width = 480.dp, height = 200.dp),
             colors = CardDefaults.cardColors(
-                containerColor = lushForestGreenDark
+                containerColor = lushForestGrassLight
             )
         ) {
-//            Text(text = "This is where the most recent cigar review is going")
+//            Text(text = "Your Most Recent Cigar Reviews")
+
+            // TODO: Examples for refreshing memory below on how objects and things are made/used
+            //  with their content. Things like this will be moved into their own functions later on
+//            var temp1 = MyCigars("Tatuaje", "Boris Karloff", "Nicaragua", 5.75f, 52)
+//            var temp2 = MyCigars("Tatuaje", "PCA 2025","Nicaragua", 5.62f, 54)
+//            var temp3 = MyCigars("Cavalier Geneve", "Green Jacket 2025", "Honduras", 7.00f, 47)
+//            val tempList = listOf<MyCigars>(
+//                temp1, temp2, temp3
+//            )
+//            filterList(tempList)
+
+//            var tempWhiskey = MyWhiskey("Maker's Mark", "46", 94f)
+//            val proofNumber = proofToPercent(tempWhiskey.proof)
+
+//            Text(text = "The most recent review was for $temp1")
+//            Text(text = "The second most recent review is $temp2")
+//            Text(text = "Will this work? Ring gauge: ${temp1.ringGauge}")
+//            Text(text = "Favorite cigar is $temp3")
+
+//            for (cigar in tempList){
+//                Text( text = "The cigar of choice is the: ${cigar.cigarName}\n")
+//            }
+
+
+            Row(
+                modifier = Modifier
+                    .padding(
+                        5.dp, // left
+                        10.dp,
+                        5.dp, // right
+                        0.dp
+                    )
+            ){
+                Card(
+//                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 225.dp, height = 180.dp)
+                        .padding(
+                            5.dp, // left
+                            0.dp,
+//                            0.dp, // right
+//                            0.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = lushForestGreenDark
+                    )
+                ) {
+                    Text(text = "This is test text")
+                }
+                Card(
+//                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 225.dp, height = 180.dp)
+                        .padding(
+                            5.dp, // left
+                            0.dp,
+//                            0.dp, // right
+//                            0.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = earthForestMediumDark
+                    )
+                ) { Text(text = "This is a test") }
+//                Card(
+////                    onClick = {},
+//                    modifier = Modifier
+//                        .size(width = 155.dp, height = 180.dp)
+//                        .padding(
+//                            5.dp, // left
+//                            0.dp,
+////                            0.dp, // right
+////                            0.dp
+//                        ),
+//                    colors = CardDefaults.cardColors(
+//                        containerColor = earthForestMediumDark
+//                    )
+//                ) { }
+            }
+
+
         }
 
         ElevatedCard(
+//            onClick = {},
             modifier = Modifier
                 .padding(
                     10.dp, // left
@@ -90,13 +165,67 @@ fun HomeScreen(){
 //                .background(color = Color.Green)
                 .size(width = 480.dp, height = 200.dp),
             colors = CardDefaults.cardColors(
-                containerColor = lushForestGreenDark
+                containerColor = lushForestGrassLight
             )
         ) {
 //            Text(text = "This is where the most recent whiskey review is going")
+
+            Row(
+                modifier = Modifier
+                    .padding(
+                        5.dp, // left
+                        10.dp,
+                        5.dp, // right
+                        0.dp
+                    )
+            ){
+                Card(
+//                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 150.dp, height = 180.dp)
+                        .padding(
+                            5.dp, // left
+                            0.dp,
+//                            0.dp, // right
+//                            0.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = earthForestMediumDark
+                    )
+                ) { }
+                Card(
+//                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 150.dp, height = 180.dp)
+                        .padding(
+                            5.dp, // left
+                            0.dp,
+//                            0.dp, // right
+//                            0.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = earthForestMediumDark
+                    )
+                ) { }
+                Card(
+//                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 150.dp, height = 180.dp)
+                        .padding(
+                            5.dp, // left
+                            0.dp,
+//                            0.dp, // right
+//                            0.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = earthForestMediumDark
+                    )
+                ) { }
+            }
         }
 
         ElevatedCard(
+//            onClick = {},
             modifier = Modifier
                 .padding(
                     10.dp, // left
@@ -107,13 +236,68 @@ fun HomeScreen(){
 //                .background(color = Color.Green)
                 .size(width = 480.dp, height = 200.dp),
             colors = CardDefaults.cardColors(
-                containerColor = lushForestGreenDark
+                containerColor = lushForestGrassLight
             )
         ) {
 //            Text(text = "This is where the list of the user's highest rated cigars is going")
+
+            Row(
+                modifier = Modifier
+                    .padding(
+                        5.dp, // left
+                        10.dp,
+                        5.dp, // right
+                        0.dp
+                    )
+            ){
+                Card(
+//                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 150.dp, height = 180.dp)
+                        .padding(
+                            5.dp, // left
+                            0.dp,
+//                            0.dp, // right
+//                            0.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = earthForestMediumDark
+                    )
+                ) { }
+                Card(
+//                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 150.dp, height = 180.dp)
+                        .padding(
+                            5.dp, // left
+                            0.dp,
+//                            0.dp, // right
+//                            0.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = earthForestMediumDark
+                    )
+                ) { }
+                Card(
+//                    onClick = {},
+                    modifier = Modifier
+                        .size(width = 155.dp, height = 180.dp)
+                        .padding(
+                            5.dp, // left
+                            0.dp,
+//                            0.dp, // right
+//                            0.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = earthForestMediumDark
+                    )
+                ) { }
+            }
+
         }
 
         ElevatedCard(
+//            onClick = {},
             modifier = Modifier
                 .padding(
                     10.dp, // left
@@ -124,11 +308,13 @@ fun HomeScreen(){
 //                .background(color = Color.Green)
                 .size(width = 480.dp, height = 260.dp),
             colors = CardDefaults.cardColors(
-                containerColor = lushForestGreenDark
+                containerColor = lushForestGrassLight
             )
         ) {
-//            Text(text = "This is potentially where a random whiskey/cigar brand will " +
-//                    "go, to give option for user to learn")
+            Text(text = "This is potentially where a random whiskey/cigar brand will " +
+                    "go, to give option for user to learn", color = Color.Black)
+            // TODO: This text is currently the same color at "lushForestGrassLight"
+            //  until I changed the color manually. Will need to keep in mind for future
         }
     }
 
