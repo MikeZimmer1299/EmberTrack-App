8dfbc562209bfc17596526c8e8ef0b4108373a40
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-10 20:42:50 -0500

    Changed objects from val to var (had done for other objects already, forgot to push these changes to the repo)

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/WhiskeyReviews.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/WhiskeyReviews.kt
@@ -1,12 +1,12 @@
 package com.example.cigarsandwhiskey.objects
 
 class WhiskeyReviews(
-    val brand: String,
-    val whiskeyName: String,
-    val type: String,
-    val proof: Int,
-    val flavors: String,
-    val aroma: String,
+    var brand: String,
+    var whiskeyName: String,
+    var type: String,
+    var proof: Int,
+    var flavors: String,
+    var aroma: String,
     // Eventually will have option to include written review
 ) {
 
