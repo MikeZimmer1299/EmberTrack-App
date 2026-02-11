398382a083406832f0e2c17f31320a96b4204c06
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-10 20:37:30 -0500

    Added extended floating button to both, to allow user to add cigars/whiskey to their collection (different from adding a new review for either)

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/MyCigarsActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/MyCigarsActivity.kt
@@ -1,17 +1,25 @@
 package com.example.cigarsandwhiskey.objectInterface
 
+import android.R
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cigarsandwhiskey.ui.theme.lushForestGrassLight
+import com.example.cigarsandwhiskey.ui.theme.lushForestGreenDark
 
 // it has the ` ` around object because my naming convention is NOT good for now
 //class MyCigarsActivity : ComponentActivity() {
@@ -37,11 +45,49 @@ fun MyCigarsScreen(){
             .padding(0.dp, 0.dp)
             .verticalScroll(rememberScrollState()),
         colors = CardDefaults.cardColors(
-            containerColor = Color(0xFF37534E)
+            containerColor = lushForestGreenDark
         )
 
     ){
-        Text(text = "Welcome to the My Cigars Page")
+//        Text(text = "Welcome to the My Cigars Page")
+
+        // TODO: First starting off as if the user only has one humidor, will implement
+        //  more than one humidor once the single humidor object is in working order
+
+        ElevatedCard(
+            modifier = Modifier
+                .padding(
+                    10.dp, // left
+                    45.dp,
+                    10.dp, // right
+                    5.dp
+                )
+//                .background(color = Color.Green)
+                .size(width = 480.dp, height = 200.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = lushForestGrassLight
+            )
+        ) {
+            Text(text = "This is where the most recent cigar added to the humidor will go" +
+                    " or it will be the option to choose which humidor you wish to view." +
+                    " This is entirely dependent on if the user has one or more humidors.")
+        }
+
     }
+
+    // TODO: This button will allow a user to add a new cigar(s) to their collection
+    ExtendedFloatingActionButton(
+        modifier = Modifier
+            .padding(
+                305.dp, // left
+                920.dp,
+                0.dp, // right
+                0.dp
+            ),
+        containerColor = lushForestGrassLight,
+        onClick = {},
+        icon = { Icon(Icons.Filled.Add, "Add Cigar Button") },
+        text = { Text(text = "Add Cigar(s)")}
+    )
 
 }
--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/MyWhiskeyActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/MyWhiskeyActivity.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cigarsandwhiskey.ui.theme.lushForestGrassLight
+import com.example.cigarsandwhiskey.ui.theme.lushForestGreenDark
 
 @Composable
 @Preview
@@ -23,10 +28,25 @@ fun MyWhiskeyScreen(){
             .padding(0.dp, 0.dp)
             .verticalScroll(rememberScrollState()),
         colors = CardDefaults.cardColors(
-            containerColor = Color(0xFF37534E)
+            containerColor = lushForestGreenDark
         )
     ){
         Text(text = "Welcome to the My Whiskey Page")
     }
+
+    // TODO: This will allow the user to add whiskey to their collection
+    ExtendedFloatingActionButton(
+        modifier = Modifier
+            .padding(
+                303.dp, // left
+                920.dp,
+                0.dp, // right
+                0.dp
+            ),
+        containerColor = lushForestGrassLight,
+        onClick = {},
+        icon = { Icon(Icons.Filled.Edit, "Add Whiskey Button") },
+        text = { Text(text = "Add Whiskey")}
+    )
 
 }
