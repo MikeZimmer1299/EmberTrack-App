477b7a4c77ccc25e20d320f7818d3f7a70d6d73f
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-10 20:25:14 -0500

    Changed colors to align with what I am hoping to create (old colors left in place), and changed theme params to remove light theme (old code remains, will remove at later time)

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/ui/theme/Color.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/ui/theme/Color.kt
@@ -9,3 +9,26 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// TODO: Custom colors for dark color scheme
+
+//val lushForestGreenDark = Color(37, 61, 44)
+//val lushForestGreenDark = Color(27, 44, 32)
+//val lushForestGreenDark = Color(0xFF434b56)
+val lushForestGrassLight = Color(0xFFC8BCAB)
+//val lushForestGreenDark = Color(0xFF37534E)
+// keeping above for background, may make slightly darker
+val forestGreenNormal = Color(46, 111, 64)
+val forestGreenLight = Color(104, 186, 127)
+val forestGreenPale = Color(207, 255, 220)
+
+val lushForestGreenDark = Color(0xFF37534E)
+// Other color palate, may choose to use this one instead
+val earthForestDark = Color(52, 78, 65)
+//val earthForestMediumDark = Color(58, 90, 64)
+//val earthForestMediumDark = Color(47, 37, 4)
+val earthForestMediumDark = Color(69, 40, 41)
+val earthForestMedium = Color(88, 129, 87)
+
+val earthForestMediumLight = Color(163, 177, 138)
+val earthForestLight = Color(218, 215, 205)
