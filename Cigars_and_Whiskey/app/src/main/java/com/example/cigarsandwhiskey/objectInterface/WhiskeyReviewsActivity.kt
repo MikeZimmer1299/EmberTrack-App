e92ebe10e5427d98857193f7a0e7b213dfeb0840
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-10 20:36:16 -0500

    Added an extended floating button to both, to allow the ability to create a new review

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/WhiskeyReviewsActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/WhiskeyReviewsActivity.kt
@@ -4,14 +4,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cigarsandwhiskey.ui.theme.lushForestGrassLight
+import com.example.cigarsandwhiskey.ui.theme.lushForestGreenDark
 
 @Composable
 @Preview
@@ -23,10 +31,25 @@ fun WhiskeyReviewsScreen(){
             .padding(0.dp, 0.dp)
             .verticalScroll(rememberScrollState()),
         colors = CardDefaults.cardColors(
-            containerColor = Color(0xFF37534E)
+            containerColor = lushForestGreenDark
         )
     ){
         Text(text = "Welcome to the Whiskey Reviews Page")
     }
+
+    // TODO: This button will allow a user to add a new whiskey review
+    ExtendedFloatingActionButton(
+        modifier = Modifier
+            .padding(
+                310.dp, // left
+                920.dp,
+                0.dp, // right
+                0.dp
+            ),
+        containerColor = lushForestGrassLight,
+        onClick = {},
+        icon = { Icon(Icons.Filled.Edit, "Add Whiskey Review Button") },
+        text = { Text(text = "Add Review")}
+    )
 
 }
