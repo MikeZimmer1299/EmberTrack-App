982a0f4633965dd730e0242aebf90e40a8a59958
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-10 20:46:52 -0500

    Same as some of the other files, excess commented code left for now to use as examples later. Will make a test file in the near future

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/MainActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/MainActivity.kt
@@ -4,44 +4,89 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
 import com.example.cigarsandwhiskey.ui.theme.CigarsAndWhiskeyTheme
+//import com.example.cigarsandwhiskey.ui.theme.MainView // in /theme folder
+// May remove the below import at a later time
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // TODO: Delete this list and val object later
+//        val people = listOf(
+//            Person("DJ", "Malone", 52),
+//            Person("DJ", "Malone", 35),
+//            Person("DJ", "Smith", 26),
+//            Person("DJ", "Malone", 21),
+//            Person("DJ", "Malone", 19)
+//        )
+//        val peopleFiltered = people.filter { it.age >= 21 && it.lastName == "Smith" }
+
         setContent {
             CigarsAndWhiskeyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppRoot()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppRoot(){ // May be a temporary name, may change later
+
+    Navigation()
+    // By calling Navigation(), it sets startDestination
+    //  to "home" screen
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CigarsAndWhiskeyTheme {
-        Greeting("Android")
-    }
-}
+// TODO: Delete at later time
+//@Composable
+//fun CardView(person: Person){ // param: Type
+//    Card(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(12.dp)
+//    ) {
+//        Row() {
+//            Image(painter = painterResource(id = R.drawable.outline_person_24),
+//                contentDescription = "Photo of person",
+//                modifier = Modifier
+//                    .width(100.dp)
+//                    .height(100.dp)
+//            )
+//            Column{
+//                Text(
+//                    text = "First Name: " + person.firstName,
+//                    modifier = Modifier.padding(top = 10.dp),
+//                )
+//                Text(
+//                    text = "Last Name: " + person.lastName,
+//                    modifier = Modifier.padding(0.dp),
+//                )
+//                Text(
+//                    text = "Age: " + person.age,
+//                    modifier = Modifier.padding(0.dp),
+//                )
+//            }
+//
+//        }
+//
+//    }
+//}
