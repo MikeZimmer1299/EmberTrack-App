1d1fbdeca7907649b6514b1f8ea20c775003c2ef
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-17 21:44:47 -0500

    Created the basis of a secondary screen which will allow a user to add cigars to their humidor

---
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/secondaryInterface/AddCigars.kt
@@ -0,0 +1,30 @@
+package com.example.cigarsandwhiskey.objectInterface.secondaryInterface
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.cigarsandwhiskey.ui.theme.lushForestGreenDark
+
+@Composable
+fun AddCigars(){
+
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp, 0.dp)
+            .verticalScroll(rememberScrollState()),
+        colors = CardDefaults.cardColors(
+            containerColor = lushForestGreenDark
+        )
+    ){
+        Text(text = "This is the screen where I will add new cigars to my collection")
+    }
+
+}
