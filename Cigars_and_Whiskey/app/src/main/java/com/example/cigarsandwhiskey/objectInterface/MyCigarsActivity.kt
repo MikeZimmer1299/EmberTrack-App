1575a977b257ffd783289dd74768828e682e97ae
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-21 19:50:32 -0500

    Removed the lines of code that would make the app 'forget' where it was before I clicked the 'Add Cigars' button, so then I can simply hit the 'Back' button on the phone to return to  screen

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/MyCigarsActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/MyCigarsActivity.kt
@@ -1,6 +1,5 @@
 package com.example.cigarsandwhiskey.objectInterface
 
-import android.R
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,7 +15,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.cigarsandwhiskey.ui.theme.lushForestGrassLight
@@ -86,11 +84,7 @@ fun MyCigarsScreen(navController: NavController){
             ),
         containerColor = lushForestGrassLight,
         onClick = {
-            navController.navigate("add_new_cigar"){
-                popUpTo(navController.graph.startDestinationId){saveState = true}
-                launchSingleTop = true
-                restoreState = true
-            }
+            navController.navigate("add_new_cigar")
         },
         icon = { Icon(Icons.Filled.Add, "Add Cigar Button") },
         text = { Text(text = "Add Cigar(s)")}
