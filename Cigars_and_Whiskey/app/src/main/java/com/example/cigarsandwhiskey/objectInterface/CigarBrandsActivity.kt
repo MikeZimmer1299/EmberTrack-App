b3e0bf37831d1ec72391233cf7c1edca4f811783
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-02-27 21:20:04 -0500

    Added a title to the screen in a Column object, making the text bold

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/CigarBrandsActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/CigarBrandsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cigarsandwhiskey.objectInterface
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -9,9 +11,13 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cigarsandwhiskey.ui.theme.lushForestGreenDark
 
 @Composable
@@ -27,7 +33,28 @@ fun CigarBrandsScreen(){
             containerColor = lushForestGreenDark
         )
     ){
-        Text(text = "Welcome to the Cigar Brands Page")
+//        Text(text = "Welcome to the Cigar Brands Page")
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp, 30.dp, 0.dp, 0.dp),
+
+//        colors = CardDefaults.cardColors()
+        ) {
+            Text(text = "Cigar Brands", fontSize = 40.sp, fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .drawBehind{
+                        val strokeWidthPx = 3.dp.toPx()
+                        drawLine(
+                            color = Color.Black,
+                            strokeWidth = strokeWidthPx,
+                            start = Offset(0f, size.height),
+                            end = Offset(size.width, size.height)
+                        )
+                    }
+            )
+        }
     }
 
 }
