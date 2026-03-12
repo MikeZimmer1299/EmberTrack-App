ce2c807451f4aefdea2f8d34caee7d6a0ce87d13
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-03-12 09:37:41 -0400

    Created a reusable input text field that allows the user to enter text for reviews or adding to their collection. While material3 does exist, I opted not to use it because the design I am shooting for does not work properly with material3 in some instances

---
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/generalFunctions/InputTextField.kt
@@ -0,0 +1,46 @@
+package com.example.cigarsandwhiskey.generalFunctions
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.ui.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InputTextField(
+    text: String,
+    onTextChange: (String) -> Unit,
+    placeholder: String
+){
+    Box(
+        modifier = Modifier
+            .background(Color.White)
+            .width(220.dp)
+            .height(40.dp)
+            .padding(0.dp, 0.dp, 0.dp, 0.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        BasicTextField(
+            value = text,
+            onValueChange = onTextChange,
+            singleLine = true,
+            textStyle = TextStyle(
+                fontSize = 20.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        if(text.isEmpty()){
+            Text(text = placeholder, color = Color.Black, fontSize = 20.sp)
+        }
+    }
+}
