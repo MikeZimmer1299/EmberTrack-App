1ffdab2dac6db3e86cfb5f9f2b8d959f0be1c854
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-03-26 17:20:05 -0400

    Same as MyWhiskey, added entity and id

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/WhiskeyReviews.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/WhiskeyReviews.kt
@@ -1,6 +1,13 @@
 package com.example.cigarsandwhiskey.objects
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "whiskey_reviews")
 class WhiskeyReviews(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+
     var brand: String,
     var whiskeyName: String,
     var type: String,
