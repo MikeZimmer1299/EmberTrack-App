4c09b62b1180c6ae27aa727e382b17ce370e4507
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-03-26 17:16:52 -0400

    Added entity to attach it to a humidor object, added id number (handled by Room)

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/MyCigars.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/MyCigars.kt
@@ -1,9 +1,28 @@
 package com.example.cigarsandwhiskey.objects
 
 import androidx.compose.runtime.Composable
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
 import com.example.cigarsandwhiskey.MainActivity
 
+@Entity(tableName = "my_cigars",
+    foreignKeys = [
+        ForeignKey(
+            entity = MyHumidor::class,
+            parentColumns = ["id"],
+            childColumns = ["humidorId"],
+            onDelete = ForeignKey.CASCADE // Deletes all cigars if humidor is deleted
+        )
+    ],
+    indices = [Index(value = ["humidorId"])]
+)
 data class MyCigars(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+
+    var humidorId: Int = 1, // default to "Main Humidor"
     var cigarBrand: String,
     var cigarName: String,
     var countryOfOrigin: String,
