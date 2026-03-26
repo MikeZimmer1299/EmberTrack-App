3810f21af5bc9deea523d7b9791b26ecc5cd5de7
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-03-26 17:07:31 -0400

    Added required dependencies to gradle files to allow local database implementation

--- Cigars_and_Whiskey/app/build.gradle.kts
+++ Cigars_and_Whiskey/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp) // used for local database storage
 }
 
 android {
@@ -58,4 +59,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Allows usage of SQL Database for local storage
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 }
