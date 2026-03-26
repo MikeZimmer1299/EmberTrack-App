90ac319b9bcbfe6e132250b1ad14ccd58f569f59
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-03-26 17:21:06 -0400

    Have not done anything major. Added NavController param and designated Card default color to match rest of UI

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/secondaryInterface/NewWhiskeyReview.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/secondaryInterface/NewWhiskeyReview.kt
@@ -5,21 +5,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+import com.example.cigarsandwhiskey.ui.theme.lushForestGreenDark
 
 
 // TODO: New secondary screen for when the user wishes to create a new
 //  review for a recently enjoyed whiskey
 @Composable
-fun NewWhiskeyReview(){
+fun NewWhiskeyReview(navController: NavController){
 
     Card(
         modifier = Modifier
             .fillMaxSize()
             .padding(0.dp)
-            .verticalScroll(rememberScrollState())
+            .verticalScroll(rememberScrollState()),
+        colors = CardDefaults.cardColors(
+            containerColor = lushForestGreenDark
+        )
     ) {
 
     // TODO: Card for Whiskey Name and Brand will be in same card
