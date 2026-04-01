ca6076ca3c584337a7c3a5377a99bfcd500f46e8
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-04-01 13:38:41 -0400

    Added the abstract DAO functions

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/AppDatabase.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/AppDatabase.kt
@@ -5,6 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.cigarsandwhiskey.dataAccessObjects.CigarReviewDao
+import com.example.cigarsandwhiskey.dataAccessObjects.MyCigarsDao
+import com.example.cigarsandwhiskey.dataAccessObjects.MyWhiskeyDao
+import com.example.cigarsandwhiskey.dataAccessObjects.WhiskeyReviewDao
 import com.example.cigarsandwhiskey.objects.CigarReviews
 import com.example.cigarsandwhiskey.objects.MyCigars
 import com.example.cigarsandwhiskey.objects.MyHumidor
@@ -20,7 +23,12 @@ import com.example.cigarsandwhiskey.objects.WhiskeyReviews
 
 abstract class AppDatabase : RoomDatabase() {
 
+    abstract fun myCigars(): MyCigarsDao
     abstract fun cigarReviewDao(): CigarReviewDao
+    abstract fun myWhiskeyDao(): MyWhiskeyDao
+    abstract fun myWhiskeyReviewDao(): WhiskeyReviewDao
+    abstract fun myHumidorsDao(): MyHumidor
+
 
     companion object{
         @Volatile // ensures value of INSTANCE is always read from main memory
