ec8ffe31b4f97e4a90f3da6c554a8cd9f59ee055
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-04-03 13:29:26 -0400

    Added @Dao

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/dataAccessObjects/MyWhiskeyDao.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/dataAccessObjects/MyWhiskeyDao.kt
@@ -1,4 +1,11 @@
 package com.example.cigarsandwhiskey.dataAccessObjects
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.cigarsandwhiskey.objects.WhiskeyReviews
+@Dao
 interface MyWhiskeyDao {
+
 }
