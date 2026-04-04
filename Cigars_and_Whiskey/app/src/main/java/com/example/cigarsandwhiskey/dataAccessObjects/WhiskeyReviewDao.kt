d2b1d27e4a6a1137acab438fa272b65fed9e6053
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-04-03 22:44:57 -0400

    Added the basic functionality to the DAO for whiskey reviews

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/dataAccessObjects/WhiskeyReviewDao.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/dataAccessObjects/WhiskeyReviewDao.kt
@@ -1,4 +1,26 @@
 package com.example.cigarsandwhiskey.dataAccessObjects
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.cigarsandwhiskey.objects.WhiskeyReviews
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface WhiskeyReviewDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertReview(reviews: WhiskeyReviews)
+
+    @Query("SELECT * FROM whiskey_reviews ORDER BY id DESC")
+    fun getAllWhiskeyReviews(): Flow<List<WhiskeyReviews>>
+
+    @Query("SELECT * FROM whiskey_reviews WHERE whiskeyName = :name LIMIT 1")
+    fun getAllWhiskeyReviews(name: String): WhiskeyReviews?
+
+    @Delete
+    suspend fun deleteReview(review: WhiskeyReviews)
+
 }
