1338bd1eea41d0021b61de9e0e8b75154f82f5fe
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-04-03 22:47:37 -0400

    Added default blank values to params for whiskey review objects

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/WhiskeyReviews.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/WhiskeyReviews.kt
@@ -4,17 +4,23 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "whiskey_reviews")
-class WhiskeyReviews(
+data class WhiskeyReviews(
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0,
 
-    var brand: String,
-    var whiskeyName: String,
-    var type: String,
-    var proof: Int,
-    var flavors: String,
-    var aroma: String,
-    // Eventually will have option to include written review
+    var brand: String = "",
+    var whiskeyName: String = "",
+    var type: String = "",
+    var origin: String = "",
+    var proof: String = "",
+    var ageStatement: String = "",
+    var flavors: String = "",
+    var aroma: String = "",
+    var mouthFeel: String = "",
+    var overallScore: String = ""
+    // Eventually will have option to include written review.
+    // This may be more directly necessary to have when the app
+    //  goes live
 ) {
 
 
