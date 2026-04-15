55db87768d572e1d859e6dff46b692f0768c8064
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-04-15 19:39:06 -0400

    Added two functions for getting all reviews from one brand and getting the most recent review from the list of reviews

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/dataAccessObjects/WhiskeyReviewDao.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/dataAccessObjects/WhiskeyReviewDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.cigarsandwhiskey.objects.CigarReviews
 import com.example.cigarsandwhiskey.objects.WhiskeyReviews
 import kotlinx.coroutines.flow.Flow
 
@@ -24,9 +25,14 @@ interface WhiskeyReviewDao {
     fun getAllWhiskeyReviews(name: String): WhiskeyReviews?
 
     // TIPS: This is for the search function, which will return
-    //  cigars by a specific brand
-    @Query("SELECT * FROM whiskey_reviews WHERE brand = :brand LIMIT 1")
-    fun getAllWhiskeyBrandReviews(brand: String): WhiskeyReviews?
+    //  cigars by a specific brand. May need to change this to a list, so then
+    //  all the cigars from a specific brand display
+    @Query("SELECT * FROM whiskey_reviews WHERE brand = :brand")
+    fun getAllWhiskeyBrandReviews(brand: String): List<WhiskeyReviews?>
+
+    // TIPS: This is intended to be used on the HomeScreen
+    @Query("SELECT * FROM whiskey_reviews ORDER BY id DESC LIMIT 1")
+    fun getMostRecentWhiskeyReview(): Flow<WhiskeyReviews?> // Flow auto-updates
 
     @Delete
     suspend fun deleteReview(review: WhiskeyReviews)
