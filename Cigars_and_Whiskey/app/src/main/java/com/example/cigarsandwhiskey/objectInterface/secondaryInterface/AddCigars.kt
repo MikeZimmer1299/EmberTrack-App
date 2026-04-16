37f06d668b1d5a31326115276397de8c8986fe38
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-04-15 21:16:15 -0400

    As far as I know, this screen is done. It's a bit empty, but with how I am formatting things, it's compact and simple. It just works

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/secondaryInterface/AddCigars.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/secondaryInterface/AddCigars.kt
@@ -1,25 +1,57 @@
 package com.example.cigarsandwhiskey.objectInterface.secondaryInterface
 
+import android.util.Log
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.cigarsandwhiskey.dataAccessObjects.MyCigarsDao
+import com.example.cigarsandwhiskey.dataAccessObjects.MyHumidorDao
+import com.example.cigarsandwhiskey.generalFunctions.DropdownMenu
+import com.example.cigarsandwhiskey.generalFunctions.InputTextField
+import com.example.cigarsandwhiskey.generalFunctions.cigarBrandsList
+import com.example.cigarsandwhiskey.generalFunctions.cigarOriginList
+import com.example.cigarsandwhiskey.objects.HumidorWithCigars
 import com.example.cigarsandwhiskey.objects.MyCigars
+import com.example.cigarsandwhiskey.specializedFunctions.ReviewWarning
+import com.example.cigarsandwhiskey.specializedFunctions.addNewCigarCompletion
+import com.example.cigarsandwhiskey.specializedFunctions.cigarReviewCompletion
 import com.example.cigarsandwhiskey.ui.theme.lushForestGrassLight
 import com.example.cigarsandwhiskey.ui.theme.lushForestGreenDark
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddCigars(
@@ -28,18 +60,28 @@ fun AddCigars(
     scope: CoroutineScope
 ){
 
+    var cigarBrand by remember { mutableStateOf("") }
+    var cigarName by remember { mutableStateOf("") }
+    var cigarOrigin by remember { mutableStateOf("") }
+
+    var openAlertDialog by remember { mutableStateOf(false) }
+
+    val focusManager = LocalFocusManager.current
+
     Card(
         modifier = Modifier
             .fillMaxSize()
             .padding(0.dp, 0.dp)
-            .verticalScroll(rememberScrollState()),
+            .verticalScroll(rememberScrollState())
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            },
         colors = CardDefaults.cardColors(
             containerColor = lushForestGreenDark
         )
-    ){
-//        Text(text = "This is the screen where I will add new cigars to my collection")
-
-        // TODO: Cigar Brand
+    ) {
         ElevatedCard(
             modifier = Modifier
                 .padding(
@@ -48,15 +90,115 @@ fun AddCigars(
                     10.dp, // right
                     5.dp
                 )
-                .size(width = 480.dp, height = 200.dp),
+                .fillMaxWidth()
+                .height(200.dp),
             colors = CardDefaults.cardColors(
                 containerColor = lushForestGrassLight
             )
         ) {
-            Text(text = "Where does this get formatted?", fontSize = 25.sp)
+            Row( // TIPS: Row for stating CIGAR BRAND
+                modifier = Modifier.padding(0.dp, 12.dp)
+            ) {
+                Text(
+                    text = "Cigar Brand:",
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(5.dp, 0.dp, 0.dp, 0.dp)
+                        .drawBehind {
+                            val strokeWidthPx = 3.dp.toPx()
+                            val verticalOffset = size.height + 2.sp.toPx()
+                            drawLine(
+                                color = Color.Black,
+                                strokeWidth = strokeWidthPx,
+                                start = Offset(0f, verticalOffset),
+                                end = Offset(size.width, verticalOffset)
+                            )
+                        }
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+
+                var cigarBrandList = cigarBrandsList()
+                DropdownMenu(
+                    cigarBrandList,
+//                    selectedIndex, // this may be unnecessary
+                    "Choose a Brand",
+                    onItemClick = { cigarBrand = cigarBrandList[it] }
+                )
+//                Log.d("Output", "Viewing the brand: $chosenBrand")
+            }
+
+            Row( // TIPS: Row for stating CIGAR NAME
+                modifier = Modifier
+                    .padding(0.dp, 12.dp)
+            ) {
+                Text(
+                    text = "Cigar Name:",
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(5.dp, 0.dp, 0.dp, 0.dp)
+                        .drawBehind {
+                            val strokeWidthPx = 3.dp.toPx()
+                            val verticalOffset = size.height + 2.sp.toPx()
+                            drawLine(
+                                color = Color.Black,
+                                strokeWidth = strokeWidthPx,
+                                start = Offset(0f, verticalOffset),
+                                end = Offset(size.width, verticalOffset)
+                            )
+                        }
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+
+                // TIPS: Text Box to enter cigar name
+                InputTextField(
+                    cigarName,
+                    onTextChange = { cigarName = it },
+                    placeholder = "Enter Cigar Name"
+                )
+            }
+
+            Row( // TIPS: Row for stating COUNTRY OF ORIGIN
+                modifier = Modifier.padding(0.dp, 12.dp)
+            ) {
+                Text(
+                    text = "Cigar Origin:",
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(5.dp, 0.dp, 0.dp, 0.dp)
+                        .drawBehind {
+                            val strokeWidthPx = 3.dp.toPx()
+                            val verticalOffset = size.height + 2.sp.toPx()
+                            drawLine(
+                                color = Color.Black,
+                                strokeWidth = strokeWidthPx,
+                                start = Offset(0f, verticalOffset),
+                                end = Offset(size.width, verticalOffset)
+                            )
+                        }
+                )
+                Spacer(modifier = Modifier.width(17.dp))
+
+                // TIPS: Dropdown menu of countries
+                var cigarOriginList = cigarOriginList()
+                DropdownMenu(
+                    cigarOriginList,
+//                    selectedIndex, // this may be unnecessary
+                    "Country of Origin",
+                    onItemClick = { cigarOrigin = cigarOriginList[it] }
+                )
+            }
         }
 
-        // TODO: Cigar Name
+        ///////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////
+
+        var cigarLength by remember { mutableStateOf("") }
+        var cigarRingGauge by remember { mutableStateOf("") }
+        var cigarQuantity by remember { mutableStateOf("") }
+
         ElevatedCard(
             modifier = Modifier
                 .padding(
@@ -66,104 +208,191 @@ fun AddCigars(
                     5.dp
                 )
 //                .background(color = Color.Green)
-                .size(width = 480.dp, height = 200.dp),
+                .fillMaxWidth()
+                .heightIn(130.dp),
             colors = CardDefaults.cardColors(
                 containerColor = lushForestGrassLight
             )
-        ){
+        ) {
+            Row( // Row for stating CIGAR LENGTH
+                modifier = Modifier.padding(0.dp, 12.dp)
+            ) {
+                Text(
+                    text = "Cigar Length:",
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(5.dp, 0.dp, 0.dp, 0.dp)
+                        .drawBehind {
+                            val strokeWidthPx = 3.dp.toPx()
+                            val verticalOffset = size.height + 2.sp.toPx()
+                            drawLine(
+                                color = Color.Black,
+                                strokeWidth = strokeWidthPx,
+                                start = Offset(0f, verticalOffset),
+                                end = Offset(size.width, verticalOffset)
+                            )
+                        }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
 
+                // TIPS: Text box for cigar length
+                InputTextField(
+                    cigarLength,
+                    onTextChange = { cigarLength = it },
+                    placeholder = "Enter Cigar Length",
+                    KeyboardType.Number
+                )
+            }
+
+
+            Row( // TIPS: Row for stating CIGAR RING GAUGE
+                modifier = Modifier.padding(0.dp, 12.dp)
+            ) {
+                Text(
+                    text = "Ring Gauge:",
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(5.dp, 0.dp, 0.dp, 0.dp)
+                        .drawBehind {
+                            val strokeWidthPx = 3.dp.toPx()
+                            val verticalOffset = size.height + 2.sp.toPx()
+                            drawLine(
+                                color = Color.Black,
+                                strokeWidth = strokeWidthPx,
+                                start = Offset(0f, verticalOffset),
+                                end = Offset(size.width, verticalOffset)
+                            )
+                        }
+                )
+
+                Spacer(modifier = Modifier.width(30.dp))
+
+                // TIPS: Text box for cigar length
+                InputTextField(
+                    cigarRingGauge,
+                    onTextChange = { cigarRingGauge = it },
+                    placeholder = "Enter Ring Gauge",
+                    KeyboardType.Number
+                )
+            }
+
+            Row(
+                modifier = Modifier.padding(0.dp, 12.dp)
+            ) {
+                Text(
+                    text = "Quantity:",
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(5.dp, 0.dp, 0.dp, 0.dp)
+                        .drawBehind {
+                            val strokeWidthPx = 3.dp.toPx()
+                            val verticalOffset = size.height + 2.sp.toPx()
+                            drawLine(
+                                color = Color.Black,
+                                strokeWidth = strokeWidthPx,
+                                start = Offset(0f, verticalOffset),
+                                end = Offset(size.width, verticalOffset)
+                            )
+                        }
+                )
+                Spacer(modifier = Modifier.width(79.dp))
+
+                InputTextField(
+                    cigarQuantity,
+                    onTextChange = { cigarQuantity = it },
+                    placeholder = "Enter # of Cigars",
+                    KeyboardType.Number
+                )
+            }
         }
 
-        // TODO: Country of Origin
-        ElevatedCard(
+        ///////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////
+
+        var newToCollection by remember { mutableStateOf(MyCigars()) }
+
+        ElevatedCard( // TIPS: Add cigars to collection
             modifier = Modifier
                 .padding(
                     10.dp, // left
                     10.dp,
                     10.dp, // right
-                    5.dp
+                    60.dp
                 )
-//                .background(color = Color.Green)
-                .size(width = 480.dp, height = 200.dp),
+                .fillMaxWidth()
+                .height(65.dp),
             colors = CardDefaults.cardColors(
                 containerColor = lushForestGrassLight
             )
-        ){
-
-        }
-
-        // TODO Length
-        ElevatedCard(
-            modifier = Modifier
-                .padding(
-                    10.dp, // left
-                    10.dp,
-                    10.dp, // right
-                    5.dp
+        ) {
+            Row {
+                Text(
+                    text = "Add to Humidor:",
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(10.dp, 7.dp, 0.dp, 0.dp)
+                        .drawBehind {
+                            val strokeWidthPx = 3.dp.toPx()
+                            val verticalOffset = size.height
+                            drawLine(
+                                color = Color.Black,
+                                strokeWidth = strokeWidthPx,
+                                start = Offset(0f, verticalOffset),
+                                end = Offset(size.width, verticalOffset)
+                            )
+                        }
                 )
-//                .background(color = Color.Green)
-                .size(width = 480.dp, height = 200.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = lushForestGrassLight
-            )
-        ){
+                Button(
+                    onClick = {
+                        newToCollection = newToCollection.copy(
+                            cigarBrand = cigarBrand,
+                            cigarName = cigarName,
+                            countryOfOrigin = cigarOrigin,
+                            sizeLength = cigarLength,
+                            ringGauge = cigarRingGauge,
+                            quantity = cigarQuantity
+                        )
+                        if (!addNewCigarCompletion(newToCollection)) {
+                            openAlertDialog = true
+                            Log.d("Review", "Review is NOT complete")
+                            // TIPS: unable to call Composable from within here, made
+                            //  a mutableStateOf boolean. cigarReviewCompletion called below
+                        } else {
+                            scope.launch {
+                                try {
+                                    myCigarsDao.insertCigar(newToCollection)
+                                    Log.d("CigarCollection", "Cigars added to collection")
 
-        }
-
-        // TODO: Ring Gauge
-        ElevatedCard(
-            modifier = Modifier
-                .padding(
-                    10.dp, // left
-                    10.dp,
-                    10.dp, // right
-                    5.dp
+                                    navController.popBackStack()
+                                } catch (e: Exception){
+                                    Log.d("Database Error", "Unable to save to collection")
+                                }
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .padding(10.dp, 10.dp, 0.dp, 0.dp)
+                        .height(45.dp),
+                    colors = ButtonDefaults.buttonColors(lushForestGreenDark)
+                ) {
+                    Text(text = "Add Cigars")
+                }
+            }
+            if(openAlertDialog){
+                ReviewWarning(
+                    onDismissRequest = { openAlertDialog = false },
+                    onConfirmation = {
+                        openAlertDialog = false
+                        println("Confirmation Registered")
+                    },
+                    dialogTitle = "Warning!",
+                    dialogText = "You have not finished adding the necessary information!"
                 )
-//                .background(color = Color.Green)
-                .size(width = 480.dp, height = 200.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = lushForestGrassLight
-            )
-        ){
-
+            }
         }
-
-        // TODO: Humidor
-        ElevatedCard(
-            modifier = Modifier
-                .padding(
-                    10.dp, // left
-                    10.dp,
-                    10.dp, // right
-                    5.dp
-                )
-//                .background(color = Color.Green)
-                .size(width = 480.dp, height = 200.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = lushForestGrassLight
-            )
-        ){
-
-        }
-
-        // TODO: Quantity
-        ElevatedCard(
-            modifier = Modifier
-                .padding(
-                    10.dp, // left
-                    10.dp,
-                    10.dp, // right
-                    65.dp
-                )
-//                .background(color = Color.Green)
-                .size(width = 480.dp, height = 200.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = lushForestGrassLight
-            )
-        ){
-
-        }
-
     }
-
 }
