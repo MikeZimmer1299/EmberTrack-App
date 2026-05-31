f0982852fbe6ddd79551b7641c5264c7e5e499a7
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-05-30 20:51:46 -0400

    Deleted unused imports

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/specializedFunctions/ReviewWarning.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/specializedFunctions/ReviewWarning.kt
@@ -2,19 +2,13 @@ package com.example.cigarsandwhiskey.specializedFunctions
 
 import androidx.compose.material3.AlertDialog
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.window.Dialog
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
