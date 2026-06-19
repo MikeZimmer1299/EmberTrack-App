f7b91bbb20c8a5f4a9c5a778fa6e1831f5c4dba1
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-06-18 20:51:44 -0400

    Added onClick navigation to DisplayWhiskeyReview

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/WhiskeyReviewsActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objectInterface/WhiskeyReviewsActivity.kt
@@ -118,6 +118,7 @@ fun WhiskeyReviewsScreen(
                             onClick = {
                                 // TODO: This will bring the user to a new screen that will
                                 //  show them all the data from their review
+                                navController.navigate("display_whiskey_review/${reviews.id}")
                             },
                             onLongClick = {
                                 deleteReview = true
