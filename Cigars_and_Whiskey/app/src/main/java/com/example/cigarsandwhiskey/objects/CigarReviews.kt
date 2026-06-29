a3b15418e8ae6bf1a6e1a61bb9ef211e4e0c2f19
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-06-29 14:44:40 -0400

    Changed it to allow it to migrate, as a change was made to the database to include notes in the cigar review

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/CigarReviews.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/objects/CigarReviews.kt
@@ -1,7 +1,6 @@
 package com.example.cigarsandwhiskey.objects
 
-import androidx.compose.runtime.Composable
-import kotlin.math.exp
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -24,65 +23,10 @@ data class CigarReviews(
     var experience: Int = 0,
     // Eventually, this will also have a written review object
     //  to attach to each review
+    @ColumnInfo(defaultValue = "")
+    var notes: String = "",
     var finalScore: Float = 0f
     // TODO: Add a slot for date
 ) {
-
-//    @Composable
-    fun setCigarReview(
-        brand: String, cigarName: String, origin: String, sizeLength: String, ringGauge: String,
-        draw: Int, burn: Int, construction: Int, flavors: Int, aroma: Int,
-        smokeProduction: Int, experience: Int, finalScore: Float
-    ){
-        this.brand = brand
-        this.cigarName = cigarName
-        this.origin = origin
-        this.sizeLength = sizeLength
-        this.ringGauge = ringGauge
-        this.draw = draw
-        this.burn = burn
-        this.construction = construction
-        this.flavors = flavors
-        this.aroma = aroma
-        this.smokeProduction = smokeProduction
-        this.experience = experience
-//        this.finalScore = overallScore(draw, burn, construction, flavors, aroma, smokeProduction, experience)
-        this.finalScore = finalScore
-    }
-
-    // TODO: This may actually be unnecessary, HOWEVER it could be useful if you want to delete
-    //  a review later. Good to clear all memory before deleting an object.
-    //  Then again, this is a garbage collector language, unlike C++ that requires that planning.
-    fun resetCigarReview(){
-        this.brand = ""
-        this.cigarName = ""
-        this.origin = ""
-        this.sizeLength = ""
-        this.ringGauge = ""
-        this.draw = 0
-        this.burn = 0
-        this.construction = 0
-        this.flavors = 0
-        this.aroma = 0
-        this.smokeProduction = 0
-        this.experience = 0
-        this.finalScore = 0f
-    }
-
-
-    // Calculates overall score of the cigar
-    // Converts all int values into float value, to allow user to see the real score
-    // TODO: this may be removed later, since the calculation is done in NewCigarReview.kt
-//    @Composable
-    fun overallScore(
-        draw: Int, burn: Int, construction: Int, flavors: Int,
-        aroma: Int, smokeProduction: Int, experience: Int
-    ) : Float {
-        val finalScore: Float = (
-                draw + burn + construction + flavors + aroma
-                        + smokeProduction + experience
-                ).toFloat() / 7f
-        return finalScore
-    }
 
 }
