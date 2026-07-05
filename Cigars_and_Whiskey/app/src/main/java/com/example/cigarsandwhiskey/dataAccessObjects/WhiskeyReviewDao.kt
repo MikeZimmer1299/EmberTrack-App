d16895cb8bba8c5318526ed7aca39e86600b0f74
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-07-05 19:39:43 -0400

    Removed unnecessary import

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/dataAccessObjects/WhiskeyReviewDao.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/dataAccessObjects/WhiskeyReviewDao.kt
@@ -5,7 +5,6 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.cigarsandwhiskey.objects.CigarReviews
 import com.example.cigarsandwhiskey.objects.WhiskeyReviews
 import kotlinx.coroutines.flow.Flow
 
