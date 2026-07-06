9566f901926ced1421a14d0db912bf65e9e1be06
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-07-06 15:28:28 -0400

    Added content to drawer to allow user to change the filter applied to the lists of reviews

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/specializedFunctions/FilterDrawer.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/specializedFunctions/FilterDrawer.kt
@@ -1,6 +1,9 @@
 package com.example.cigarsandwhiskey.specializedFunctions
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ModalDrawerSheet
@@ -8,13 +11,24 @@ import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
+
+import com.example.cigarsandwhiskey.generalFunctions.DropdownMenu
+import com.example.cigarsandwhiskey.generalFunctions.InputTextField
+import com.example.cigarsandwhiskey.generalFunctions.cigarBrandsList
+import com.example.cigarsandwhiskey.generalFunctions.cigarOriginList
+import com.example.cigarsandwhiskey.generalFunctions.whiskeyBrandsList
+import com.example.cigarsandwhiskey.generalFunctions.whiskeyOriginList
 
 /*
 * TIPS: Below is a new concept: Slot API
@@ -24,6 +38,7 @@ import kotlinx.coroutines.launch
 */
 @Composable
 fun CigarReviewFilter(
+    onFilterChange: (FilterType, String) -> Unit,
     // TIPS: Accepts composable screen and provides a trigger to open the drawer.
     //  `openFilter` is its own function, so the `()` is able to take params
     content: @Composable (openFilter: () -> Unit) -> Unit
@@ -35,6 +50,9 @@ fun CigarReviewFilter(
 
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+
+    var filterQuery by remember { mutableStateOf("") }
+    var cigarNameQuery by remember { mutableStateOf("") }
 
     ModalNavigationDrawer(
         drawerState = drawerState,
@@ -57,6 +75,69 @@ fun CigarReviewFilter(
                 //  Cigar brand, based on dropdown list
                 //  Cigar name, based on user's text input
                 //  Country of Origin, based on dropdown list
+
+                Button(
+                    onClick = {
+                        onFilterChange(FilterType.ALL, "")
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.width((screenWidth * .24f).dp)
+                ) {
+                    Text(text = "Reset")
+                }
+                Row(){
+                    val cigarBrandsList = cigarBrandsList()
+                    DropdownMenu(
+                        itemList = cigarBrandsList,
+                        initialText = "Choose a Band",
+                        modifier = Modifier.weight(screenWidth * .0011f).padding(5.dp, 3.dp),
+                        onItemClick = {filterQuery = cigarBrandsList[it]}
+                    )
+                }
+                Button(
+                    onClick = {
+                        onFilterChange(FilterType.BRAND, filterQuery)
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.width((screenWidth * .24f).dp)
+                ) {
+                    Text(text = "Brand")
+                }
+                Row(){
+                    InputTextField(
+                        cigarNameQuery,
+                        onTextChange = {cigarNameQuery = it},
+                        modifier = Modifier.weight(screenWidth * .0011f).padding(5.dp, 3.dp),
+                        placeholder = "Enter Cigar Name"
+                    )
+                }
+                Button(
+                    onClick = {
+                        onFilterChange(FilterType.NAME, cigarNameQuery)
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.width((screenWidth * .24f).dp)
+                ) {
+                    Text(text = "Name")
+                }
+                Row(){
+                    val cigarOriginsList = cigarOriginList()
+                    DropdownMenu(
+                        itemList = cigarOriginsList,
+                        initialText = "Country of Origin",
+                        modifier = Modifier.weight(screenWidth * .0011f).padding(5.dp, 3.dp),
+                        onItemClick = {filterQuery = cigarOriginsList[it]}
+                    )
+                }
+                Button(
+                    onClick = {
+                        onFilterChange(FilterType.COUNTRY, filterQuery)
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.width((screenWidth * .24f).dp)
+                ) {
+                    Text(text = "Country")
+                }
             }
         }
     ) {
@@ -72,6 +153,7 @@ fun CigarReviewFilter(
 // TIPS: Same for above, but for Whiskey Reviews
 @Composable
 fun WhiskeyReviewFilter(
+    onFilterChange: (FilterType, String) -> Unit,
     content: @Composable (openFilter: () -> Unit) -> Unit
 ){
 
@@ -81,6 +163,9 @@ fun WhiskeyReviewFilter(
 
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+
+    var filterQuery by remember { mutableStateOf("") }
+    var whiskeyNameQuery by remember { mutableStateOf("") }
 
     ModalNavigationDrawer(
         drawerState = drawerState,
@@ -103,6 +188,69 @@ fun WhiskeyReviewFilter(
                 //  Whiskey brand, based on dropdown list
                 //  Whiskey name, based on user's text input
                 //  Country of Origin, based on dropdown list
+
+                Button(
+                    onClick = {
+                        onFilterChange(FilterType.ALL, "")
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.width((screenWidth * .24f).dp)
+                ) {
+                    Text(text = "Reset")
+                }
+                Row(){
+                    val whiskeyBrandsList = whiskeyBrandsList()
+                    DropdownMenu(
+                        itemList = whiskeyBrandsList(),
+                        initialText = "Choose a Band",
+                        modifier = Modifier.weight(screenWidth * .0011f).padding(5.dp, 3.dp),
+                        onItemClick = {filterQuery = whiskeyBrandsList[it]}
+                    )
+                }
+                Button(
+                    onClick = {
+                        onFilterChange(FilterType.BRAND, filterQuery)
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.width((screenWidth * .24f).dp)
+                ) {
+                    Text(text = "Brand")
+                }
+                Row(){
+                    InputTextField(
+                        whiskeyNameQuery,
+                        onTextChange = {whiskeyNameQuery = it},
+                        modifier = Modifier.weight(screenWidth * .0011f).padding(5.dp, 3.dp),
+                        placeholder = "Enter Whiskey Name"
+                    )
+                }
+                Button(
+                    onClick = {
+                        onFilterChange(FilterType.NAME, whiskeyNameQuery)
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.width((screenWidth * .24f).dp)
+                ) {
+                    Text(text = "Name")
+                }
+                Row(){
+                    val whiskeyOriginsList = whiskeyOriginList()
+                    DropdownMenu(
+                        itemList = whiskeyOriginsList,
+                        initialText = "Country of Origin",
+                        modifier = Modifier.weight(screenWidth * .0011f).padding(5.dp, 3.dp),
+                        onItemClick = {filterQuery = whiskeyOriginsList[it]}
+                    )
+                }
+                Button(
+                    onClick = {
+                        onFilterChange(FilterType.COUNTRY, filterQuery)
+                        scope.launch { drawerState.close() }
+                    },
+                    modifier = Modifier.width((screenWidth * .24f).dp)
+                ) {
+                    Text(text = "Country")
+                }
             }
         }
     ) {
@@ -110,6 +258,5 @@ fun WhiskeyReviewFilter(
         content {
             scope.launch { drawerState.open() }
         }
-
     }
 }
