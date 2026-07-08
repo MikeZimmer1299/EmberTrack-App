b1f55883d5720e46a8400dc0a116ea476b8a62cd
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-07-08 16:51:09 -0400

    WindowCompat added to keep textfields in view of user as they type

--- Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/MainActivity.kt
+++ Cigars_and_Whiskey/app/src/main/java/com/example/cigarsandwhiskey/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.core.view.WindowCompat
 
 import com.example.cigarsandwhiskey.ui.theme.CigarsAndWhiskeyTheme
 
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         val database = AppDatabase.getDatabase(this)
 
